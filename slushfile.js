97c77390a9fe9ba68007522b09ab359308b95564
Ben Teichman <ben.l.teichman@gmail.com>
2017-02-02 16:44:13 -0500

    feat($initial): scaffold client generator

---
+++ slushfile.js
@@ -0,0 +1,86 @@
+/*
+ * slush-groupby-client
+ * https://github.com/groupby/slush-client
+ *
+ * Copyright (c) 2017, GroupBy Inc.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+var conflict = require('gulp-conflict');
+var template = require('gulp-template');
+var rename = require('gulp-rename');
+var install = require('gulp-install');
+var _ = require('underscore.string');
+var inquirer = require('inquirer');
+var path = require('path');
+
+var defaults = (function () {
+  var workingDirName = path.basename(process.cwd());
+  var homeDir;
+
+  if (process.platform === 'win32') {
+    homeDir = process.env.USERPROFILE;
+  } else {
+    homeDir = process.env.HOME || process.env.HOMEPATH;
+  }
+
+  var configFile = path.join(homeDir, '.gitconfig');
+  var user = {};
+
+  if (require('fs').existsSync(configFile)) {
+    user = require('iniparser').parseSync(configFile).user;
+  }
+
+  return {
+    serviceName: workingDirName.split('-')[0],
+    authorName: user.name || '',
+    authorEmail: user.email || ''
+  };
+})();
+
+gulp.task('default', function (done) {
+  var prompts = [{
+    name: 'serviceName',
+    message: 'What is the name of your microservice?',
+    default: defaults.serviceName
+  }, {
+    name: 'appDescription',
+    message: 'What is the description?'
+  }, {
+    name: 'authorName',
+    message: 'What is the author name?',
+    default: defaults.authorName
+  }, {
+    name: 'authorEmail',
+    message: 'What is the author email?',
+    default: defaults.authorEmail
+  }, {
+    type: 'confirm',
+    name: 'moveon',
+    message: 'Continue?'
+  }];
+    // Ask
+  inquirer.prompt(prompts)
+  .then(function (answers) {
+    if (!answers.moveon) {
+      return done();
+    }
+    answers.appName = `${_.slugify(answers.serviceName)}-client-javascript`;
+    gulp.src(path.join(__dirname, 'templates', '**', '*'))
+      .pipe(template(answers))
+      .pipe(rename(function (file) {
+        if (file.basename[0] === '_') {
+          file.basename = '.' + file.basename.slice(1);
+        }
+      }))
+      .pipe(conflict('./'))
+      .pipe(gulp.dest('./'))
+      .pipe(install())
+      .on('end', function () {
+        done();
+      });
+  });
+});
